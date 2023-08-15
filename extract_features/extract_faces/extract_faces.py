1060e623295c7d58ead0ed4d159bac7b2af88221
Amelia Harrison <aharrison@ucsb.edu>
2023-08-15 22:28:37 +0000

    Adding pliers face extractor with sample video clips.

--- extract_features/extract_faces/extract_faces.py
+++ extract_features/extract_faces/extract_faces.py
@@ -1,7 +1,19 @@
 '''
 Feature extraction: extract faces
     Use the face_recognition package in the pliers package to for automated extraction of features from .mp4 data.
+    
 '''
+
+# Install pliers_extractors
+from pliers.extractors import FaceRecognitionFaceLocationsExtractor
+from os.path import join
+from pliers.tests.utils import get_test_data_path
+
+# A test picture
+image = ()
+
+# Initialize extractor
+ext = FaceRecognitionFaceLocationsExtractor()
 
 
 
